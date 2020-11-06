fb4aaf0dbfaf85d437435cc1a2170c2ba58a31db
TimurSagaidakov <66359248+TimurSagaidakov@users.noreply.github.com>
2020-11-06 21:32:47 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,16 @@
-
-    let e = document.querySelector('.text')
-    /*const fadeIn = (e) =>{
-      let opacity = 0.01
-      e.innerHTML +=  ` <div class="added-text fading" style="opacity:${opacity}">${newText}</div>`
-      let timer = setInterval(()=>{
-        if(opacity >=1){
-          clearInterval(timer)
-        }
-        let arr = document.querySelectorAll('.added-text') 
-        arr[arr.length-1].style.opacity = opacity
-        opacity += opacity * 0.1
-      }, 30)
-    } */
-    function checkKey() {
-      if( (event.keyCode >=1072 && event.keyCode <= 1103)|| (event.keyCode >=97 && event.keyCode <= 122)){
-        newText = event.key      
-        //fadeIn(e)
-        e.innerHTML +=  ` <div class="added-text fading" >${newText}</div>`    
-        let addedText = document.querySelectorAll('.fading')/* */
-        /*for(let i = 0 ;i<addedText.length; i++){
-          let style = window.getComputedStyle(addedText[i])
-          if(style.opacity === '1'){
-            addedText[i].classList.remove('fading')
-          }
-        } */
-        if(addedText.length>1){
-          addedText[0].classList.remove('fading')
-        } 
-        /*setTimeout(()=>{
-        },1) */
-      }
-    }
+let e = document.querySelector('.text')
+let cursor = document.querySelector('.cursor')
+function checkKey() {
+  if( (event.keyCode >=1072 && event.keyCode <= 1103)|| (event.keyCode >=97 && event.keyCode <= 122)){
+    newText = event.key      
+    e.innerHTML +=  ` <div class="added-text fading" >${newText}</div>`    
+    window.scrollTo({
+      top: 1000,
+      behavior: "smooth"
+  });
+    let addedText = document.querySelectorAll('.fading')/* */
+    if(addedText.length>1){
+      addedText[0].classList.remove('fading')
+    } 
+  }
+}
